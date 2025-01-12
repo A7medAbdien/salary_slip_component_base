db0492f72329635a86beaa1a9bd9a396d02b17db
A7medAbdien <ahmed.g.abdien@gmail.com>
2025-01-12 18:39:26 +0300

    Create a new assignment on updating the structure in the employee_grade documentation need to be fixed

--- salary_slip_component_base/events/employee_grade.py
+++ salary_slip_component_base/events/employee_grade.py
@@ -8,6 +8,7 @@ def on_update(doc, event):
     validate_default_salary_structure(doc, event)
     update_employee_salary_structure_assignemnt(doc, event)
 
+
 def validate_default_salary_structure(doc, event):
     """
     Validates Default Salary Structure.
@@ -54,7 +55,61 @@ def validate_default_salary_structure(doc, event):
 
 def update_employee_salary_structure_assignemnt(doc, event):
     """
-    Updates Employee Salary Structure Assignment on changing
-    the Default Salary Structure to a valid one.
+    Updates Employee "Salary Structure Assignment" on changing
+    the "Default Salary Structure" to the new "Default Salary Structure".
+    - Step 1: get the previous version of the doc, to get the previous Structure
+    Case A: for some reason got assigned to a different Structure
+        - Step 3: get all employee with this grade
+        - Step 4: create a new Assignment with the new Structure
+    Case B: different
+        - Step 5: Get all Assignment with the previous Structure
+        - Step 6: if the Assignment not equal to the current Structure,
+        cancel and create a new Assignment with the new Structure
     """
-    pass
+    # Step 1: get the previous version of the doc
+    previous_doc = doc.get_doc_before_save()
+    # Step 2: Check if the previous Structure different from the current
+    if previous_doc.default_salary_structure != doc.default_salary_structure:
+        # Step 3: Get all Assignment with the previous Structure
+        ssas = frappe.db.get_list(
+            "Salary Structure Assignment",
+            filters={
+                "salary_structure": previous_doc.default_salary_structure,
+                "grade": doc.name
+            })
+        print(f"\n\n\n ssas: {ssas} \n\n\n")
+
+        if len(ssas) == 0:
+            #Step 3: get all employee with this grade
+            emps = frappe.db.get_list(
+                "Employee",
+                filters={"grade": doc.name}
+            )
+            for emp in emps:
+                # Step 4: create a new Assignment with the new Structure
+                ssa = frappe.get_doc({
+                    "doctype": "Salary Structure Assignment",
+                    "employee": emp.name,
+                    "salary_structure": doc.default_salary_structure,
+                    "from_date": frappe.utils.nowdate(),
+                    "company": emp.company,
+                })
+                ssa.insert()
+                ssa.submit()
+
+        for ssa in ssas:
+            ssa = frappe.get_doc("Salary Structure Assignment", ssa.name)
+            if ssa.salary_structure == doc.default_salary_structure:
+                continue
+            ssa.cancel()
+            print(f"\n\n\n ssa: {ssa.as_dict()} \n\n\n")
+            # Step 4: Create a new Assignment with the new Structure
+            new_assignment = frappe.get_doc({
+                "doctype": "Salary Structure Assignment",
+                "employee": ssa.employee,
+                "salary_structure": doc.default_salary_structure,
+                "from_date": frappe.utils.nowdate(),
+                "company": ssa.company,
+            })
+            new_assignment.insert()
+            new_assignment.submit()
