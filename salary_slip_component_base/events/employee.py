374b9471e37eb867f13f3986ba10d135baa130cc
A7medAbdien <ahmed.g.abdien@gmail.com>
2025-01-12 17:15:42 +0300

    fix - consider creating employee without a salary structure assignament

--- salary_slip_component_base/events/employee.py
+++ salary_slip_component_base/events/employee.py
@@ -3,50 +3,52 @@ import frappe
 
 def on_update(doc, event):
     """
-    Update the salary structure assignment on grade update.
+    Update the salary structure assignment when an employee's grade is updated.
 
     Steps:
-    - Get salary structure based on employee grade.
-    - Get current salary structure assignment.
-    - Check if salary structure is different.
-    - If different, then cancel the previous salary structure assignment.
-    - Create a new salary structure assignment with the new salary structure.
+    1. Get the salary structure based on the employee grade.
+    2. Check if there is a current salary structure assignment:
+       Case A: If there is a current assignment:
+           A.1. Retrieve the current salary structure assignment.
+           A.2. If different, cancel the previous salary structure assignment.
+    3. Create a new salary structure assignment with the updated salary structure.
     """
-    # this is to prevent the infinit loop on update
+    # Prevent infinite loop on update
     if getattr(doc, "_on_update_handled", False):
         return
     doc._on_update_handled = True
-
     if doc.grade:
         # Step 1: Get salary structure based on employee grade
         employee_grade = frappe.get_doc("Employee Grade", doc.grade)
         if not employee_grade:
             frappe.throw(
-                "No Employee grade found consider crateing an Employee Grade")
+                "No Employee Grade found. Please create an Employee Grade.")
         new_salary_structure = employee_grade.default_salary_structure
         if not new_salary_structure:
             frappe.throw(
-                msg="No defaulr salary structure found for grade {0}".format(doc.grade))
-        print(f"\n\n\n new_salary_structure: {new_salary_structure} \n\n\n")
-
+                msg="No default salary structure found for grade {0}. Please set a default salary structure.".format(
+                    doc.grade)
+            )
+        print(f"New salary structure: {new_salary_structure}")
         # Step 2: Get current salary structure assignment
-        current_assignment = frappe.get_doc("Salary Structure Assignment", {
-                                            "employee": doc.name, "docstatus": 1})
-        print(f"\n\n\n current_assignment: {current_assignment} \n\n\n")
-
-        # Step 3: Check if salary structure is different
-        if current_assignment.salary_structure != new_salary_structure:
-
-            # Step 4: Cancel the previous salary structure assignment
-            current_assignment.cancel()
-
-            # Step 5: Create a new salary structure assignment with the new salary structure
-            new_assignment = frappe.get_doc({
-                "doctype": "Salary Structure Assignment",
-                "employee": doc.name,
-                "salary_structure": new_salary_structure,
-                "from_date": frappe.utils.nowdate(),
-                "company": doc.company,
-            })
-            new_assignment.insert()
-            new_assignment.submit()
+        current_assignments = frappe.db.get_list(
+            "Salary Structure Assignment",
+            filters={"employee": doc.name, "docstatus": 1}
+        )
+        print(f"Current assignment: {current_assignments}")
+        if len(current_assignments) > 0:
+            current_assignment = current_assignments[0]
+            # Step 3: Check if salary structure is different
+            if current_assignment and current_assignment.salary_structure != new_salary_structure:
+                # Step 4: Cancel the previous salary structure assignment
+                current_assignment.cancel()
+        # Step 5: Create a new salary structure assignment with the new salary structure
+        new_assignment = frappe.get_doc({
+            "doctype": "Salary Structure Assignment",
+            "employee": doc.name,
+            "salary_structure": new_salary_structure,
+            "from_date": frappe.utils.nowdate(),
+            "company": doc.company,
+        })
+        new_assignment.insert()
+        new_assignment.submit()
